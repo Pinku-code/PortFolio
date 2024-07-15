ce82bd54b9fdd331c9316eaa5c856383d0b52dad
Pinku-code <ns4507262@gmail.com>
2024-07-15 12:32:17 +0530

    Update Section

---
+++ src/sections/Blog/Blog.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import BlogCard from './BlogCard';
+import './Styles.css';
+
+function Blog() {
+  const blogs = [
+    {
+      title: "Blog Post 1",
+      image: "https://via.placeholder.com/300",
+      description: "This is a short description for blog post 1."
+    },
+    {
+      title: "Blog Post 2",
+      image: "https://via.placeholder.com/300",
+      description: "This is a short description for blog post 2."
+    },
+    {
+      title: "Blog Post 3",
+      image: "https://via.placeholder.com/300",
+      description: "This is a short description for blog post 3."
+    },
+    {
+      title: "Blog Post 3",
+      image: "https://via.placeholder.com/300",
+      description: "This is a short description for blog post 3."
+    },
+    {
+      title: "Blog Post 3",
+      image: "https://via.placeholder.com/300",
+      description: "This is a short description for blog post 3."
+    },
+    {
+      title: "Blog Post 3",
+      image: "https://via.placeholder.com/300",
+      description: "This is a short description for blog post 3."
+    },
+    {
+      title: "Blog Post 3",
+      image: "https://via.placeholder.com/300",
+      description: "This is a short description for blog post 3."
+    }
+  ];
+
+  return (
+    <div className="blog-container">
+      {blogs.map((blog, index) => (
+        <BlogCard 
+          key={index}
+          title={blog.title}
+          image={blog.image}
+          description={blog.description}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Blog;
---
+++ src/sections/Blog/BlogCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Styles.css';
+
+function BlogCard({ title, image, description }) {
+  return (
+    <div className="blog-card">
+      <img src={image} alt={title} />
+      <div className="content">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+BlogCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default BlogCard;
