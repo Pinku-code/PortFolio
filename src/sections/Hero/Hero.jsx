7490297087fc3d7293e5752cb204d7e1141e338d
Pinku-code <ns4507262@gmail.com>
2024-07-17 16:40:31 +0530

    Update Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,20 +2,17 @@ import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/DP.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
-// import twitterLight from '../../assets/twitter-light.svg';
-// import twitterDark from '../../assets/twitter-dark.svg';
 import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/cv.pdf';
+import CV from '../../assets/cv.txt';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === 'light' ? sun : moon;
-  // const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
@@ -27,12 +24,12 @@ function Hero() {
           className={styles.hero}
           alt="Profile picture of Abinash"
         />
-        <img
-          className={styles.colorMode}
-          src={themeIcon}
-          alt="Color mode icon"
+        <div
+          className={`${styles.colorMode} ${theme === 'dark' ? styles.on : ''}`}
           onClick={toggleTheme}
-        />
+        >
+          <div className={styles.colorModeCircle}></div>
+        </div>
       </div>
       <div className={styles.info}>
         <h1>
@@ -40,13 +37,10 @@ function Hero() {
         </h1>
         <h3>Software Developer</h3>
         <span>
-          {/* <a href="https://twitter.com/" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
-          </a> */}
-          <a href="https://github.com/Pinku-code" target="_blank">
+          <a href="https://github.com/Pinku-code" target="_blank" rel="noopener noreferrer">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
@@ -54,7 +48,7 @@ function Hero() {
             Experienced software developer<br/> skilled in the <i className="fab fa-leaf"></i> <b style={{color:"#009ff8"}}>MERN</b> stack.
         </p>
         <a href={CV} download>
-          <button className="hover">Resume</button>
+          <button className={styles.hover}>Resume</button>
         </a>
       </div>
     </section>
